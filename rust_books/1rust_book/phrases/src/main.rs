d048ae07fe3700133078ff01753c256e183322b8
xftroxgpx <xftroxgpx@gmail.com>
2017-04-26 22:56:29 +0200

    'use as'

--- rust_books/1rust_book/phrases/src/main.rs
+++ rust_books/1rust_book/phrases/src/main.rs
@@ -1,12 +1,16 @@
 extern crate phrases;
+extern crate phrases as sayings;
 
 use phrases::english::{greetings,farewells};
 use phrases::japanese;//it's different than 'english' see its mod.rs
+//use sayings::japanese::{self};//equivalent with the above ^
+use sayings::japanese::greetings::hello as hi2;
 
 fn main() {
     println!("Hello in English: {}", greetings::hello());
     println!("Goodbye in English: {}", farewells::goodbye());
 
-    println!("Hello in Japanese: {}", japanese::hello());
+    println!("Hello in Japanese: {}", japanese::hi());
+    println!("Hello in Japanese: {}", hi2());
     println!("Goodbye in Japanese(日本語): {}", japanese::goodbye());
 }
