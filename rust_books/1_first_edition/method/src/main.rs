082ac6174b65e0cf921c4aafea280001c78d96eb
xftroxgpx <xftroxgpx@gmail.com>
2017-05-03 13:49:03 +0200

    method calls: try non-references
    and try associated function call

--- rust_books/1_first_edition/method/src/main.rs
+++ rust_books/1_first_edition/method/src/main.rs
@@ -9,6 +9,10 @@ impl Circle {
         std::f64::consts::PI * (self.radius * self.radius)
     }
 
+    fn area2(self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+
     //static method
     fn new(x: f64, y: f64, radius: f64) -> Circle {
         Circle {
@@ -26,4 +30,10 @@ fn main() {
     //access static method via Circle:: not c.
     let c = Circle::new(0.0, 0.0, 2.0);
     println!("{}", c.area());
+    println!("{}", c.area2());
+//    println!("{}", Circle::area(&c));//moved
+    let d = Circle::new(0.0, 0.0, 3.0);
+    println!("{}", Circle::area(&d));
+    println!("{}", Circle::area2(d));
+//    println!("{}", d.area());//moved!
 }
