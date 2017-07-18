5fd2a0132da4696cbc1e0e17aee2ac37d63cf087
xftroxgpx <xftroxgpx@gmail.com>
2017-07-18 18:15:05 +0200

    update intrinsics / get type

--- rust_sandbox/get_type/src/main.rs
+++ rust_sandbox/get_type/src/main.rs
@@ -1,15 +1,21 @@
 //source: https://stackoverflow.com/questions/21747136/how-do-i-print-the-type-of-a-variable-in-rust
+#![feature(core_intrinsics)]
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", unsafe { std::intrinsics::type_name::<T>() });
+}
+/* old variant:
 fn print_type_of<T>(_: &T) -> () {
     let type_name =
         unsafe {
             (*std::intrinsics::get_tydesc::<T>()).name
         };
     println!("{}", type_name);
-}
+}*/
 
 fn main() -> () {
     //mixing range into this
-    for i in range(-3,10) {//falls back to i32 regardless of numbers https://github.com/rust-lang/rust/issues/21595
+    for i in -3..10 {//falls back to i32 regardless of numbers https://github.com/rust-lang/rust/issues/21595
         if i % 9 == 0 {// -1%9==0
             print_type_of(&i);
         }
