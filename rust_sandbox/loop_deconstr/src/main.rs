06077e418b5ab2f398671dceca04b8230d294916
xftroxgpx <xftroxgpx@gmail.com>
2017-07-25 05:09:16 +0200

    no mem changes

---
+++ rust_sandbox/loop_deconstr/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    loop {
+    loop {
+        //no change in VIRT  26.6m   5.9m RES
+        let mut a=String::with_capacity(10000000);
+        print!("{}",a);
+        //a.push('a');
+        break;
+    }
+    }
+}
