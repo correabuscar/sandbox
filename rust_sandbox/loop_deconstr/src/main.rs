cdcba317fcbeb1bf42e7d7797ac41b236c76adb2
xftroxgpx <xftroxgpx@gmail.com>
2017-07-25 05:28:21 +0200

    it is dropped indeed

--- rust_sandbox/loop_deconstr/src/main.rs
+++ rust_sandbox/loop_deconstr/src/main.rs
@@ -1,11 +1,20 @@
+
+struct Soo(String);
+
+impl Drop for Soo {
+    fn drop(&mut self) {
+        //println!("!drop!");//it is dropped!
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     loop {
     loop {
         //no change in VIRT  26.6m   5.9m RES
-        let mut a=String::with_capacity(10000000);
-        print!("{}",a);
-        //a.push('a');
+        let mut a: Soo=Soo(String::with_capacity(10000000));
+        print!("{}",a.0);
+        //a.0.push('a');
         break;
     }
     }
