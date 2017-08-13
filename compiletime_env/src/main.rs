e03ecd462e45714c8ac55263f15b1bf42d6e09b6
xftroxgpx <xftroxgpx@protonmail.com>
2017-08-13 16:26:18 +0200

    CARGO_MANIFEST_DIR is what I want

--- compiletime_env/src/main.rs
+++ compiletime_env/src/main.rs
@@ -1,4 +1,15 @@
-const PWD_AT_COMPILETIME: &'static str = env!("PWD");
+//use std::env;
+
+// eg. /home/xftroxgpx/build/2nonpkgs/rust.stuff/rustlearnage/compiletime_env
+const PWD_AT_COMPILETIME: &'static str = env!("CARGO_MANIFEST_DIR");
+
 fn main() {
+
+    //all envs at runtime
+    for (key, value) in std::env::vars() {
+        println!("{}: {}", key, value);
+    }
+
+    //one compile time env:
     println!("Hello, world! {}", PWD_AT_COMPILETIME);
 }
