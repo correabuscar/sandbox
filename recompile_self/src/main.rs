5f1d998bb1dc54f34ac179ab1e1708c0d392d958
xftroxgpx <xftroxgpx@protonmail.com>
2017-08-13 22:37:17 +0200

    self-compiling executable using cargo/Rust
    code thus far

--- recompile_self/src/main.rs
+++ recompile_self/src/main.rs
@@ -2,7 +2,6 @@
 extern crate log;
 extern crate filetime;
 
-use std::io::Write; //XXX: needed for flush() to be seen in scope!
 
 //use std::env;
 
@@ -35,21 +34,12 @@ macro_rules! fflush {
 
 fn main() {
 
-    //all envs at runtime
-    /*    for (key, value) in std::env::vars() {
-          println!("{}: {}", key, value);
-          }*/
-
-    //one compile time env:
-    println!("Hello, world! CARGO_MANIFEST_DIR={}", PWD_AT_COMPILETIME);
-    //println!("{}", env!("CARGO_TARGET_DIR"));
-    //println!("{}", env!("OUT_DIR"));
 
     // FIXME: find better way to detect main.rs and others
     let sources = [std::path::Path::new(&PWD_AT_COMPILETIME).join("src/main.rs")];
     //TODO: use * glob to find all *.rs in src/ ! or something
 
-    //TODO: detect if source changed!
+    // detect if source changed!
     let exe_full_name=std::env::current_exe().unwrap();
     let metadata0 = std::fs::metadata(&exe_full_name).unwrap();
     let mtime0 = filetime::FileTime::from_last_modification_time(&metadata0);
@@ -73,14 +63,14 @@ fn main() {
     }
 
     if changed {
-        print!("Recompiling executable due to source changed...");
+        eprint!("!! Recompiling executable due to source changed...");
         //std::io::stdout().flush().ok().expect("Could not flush stdout");
         //fflush!(std::io::stdout());
         fflush!();
         let args=vec!["build","-v",
         #[cfg(not(debug_assertions))]
         "--release"
-        ]; //FIXME: replace with 'run' so we don't have to manually also run it below!
+        ]; //XXX: nvm: replace with 'run' so we don't have to manually also run it below! Actually NO, because then we have to show stdout/stderr from compilation too!
         /*if !CARGO_MODE.is_empty() {
             args.push(CARGO_MODE);
         }*/
@@ -92,11 +82,11 @@ fn main() {
             .output()
             .expect("failed to execute process");
         if output.status.success() {
-            println!("all good! {}", output.status);
+            eprintln!("all good! {}", output.status);
         }else{
-            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-            println!("status: {}", output.status);
+            eprintln!("failed! {}", output.status);
+            println!("!! stdout: {}", String::from_utf8_lossy(&output.stdout));
+            println!("!! stderr: {}", String::from_utf8_lossy(&output.stderr));
         }
         //prevent possible endless loop by making sure the timestamps for the old exe and new exe
         //are now different due to compilation placing the new exe in the same place!
@@ -116,4 +106,13 @@ fn main() {
     }
 
     println!("Exe fname is {:?}", exe_full_name);
+    //all envs at runtime
+    /*    for (key, value) in std::env::vars() {
+          println!("{}: {}", key, value);
+          }*/
+
+    //one compile time env:
+    println!("Hello, world! CARGO_MANIFEST_DIR={}", PWD_AT_COMPILETIME);
+    //println!("{}", env!("CARGO_TARGET_DIR"));
+    //println!("{}", env!("OUT_DIR"));
 }
