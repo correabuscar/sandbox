7a40416434a34f6bccf94dcb2b45591feddeb02a
xftroxgpx <xftroxgpx@protonmail.com>
2017-08-13 22:46:31 +0200

    trace_macros!(true) and log_syntax!

--- macra/src/main.rs
+++ macra/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(trace_macros)]
+#![feature(log_syntax)]
 //use std::io::Write; //XXX: needed for flush() to be seen in scope!
 //macro_rules! fflush { () => ({ std::io::stdout().flush().ok().expect(stringify!(Could not flush $name)); }) }
 macro_rules! moosh { ($x:expr) => ({
@@ -10,7 +12,7 @@ macro_rules! fflush {
     });
     ($stdwhat:ident) => ({
 		use std::io::Write; //XXX: needed for flush() to be seen in scope!
-		std::io::$stdwhat().flush().ok().expect(stringify!(Could not flush $stdwhat)); });
+		std::io::$stdwhat().flush().ok().expect(stringify!(Could not moosh $stdwhat)); });
 }
 
 
@@ -20,7 +22,10 @@ fn main() {
         use std::io::Write; //XXX: needed for flush() to be seen in scope!
         moosh!(std::io::stdout().flush().ok());
     }
+    trace_macros!(true); //src: file:///home/xftroxgpx/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/share/doc/rust/html/book/first-edition/macros.html#debugging-macro-code
 	fflush!();
+    trace_macros!(false);
+	log_syntax!(fflush!());//FIXME: looks like this does nothing?
 	fflush!(stdout);
 	fflush!(stderr);
     moosh!(x);//good: thread 'main' panicked at 'Could not flush x: 102', src/libcore/result.rs:860:4
