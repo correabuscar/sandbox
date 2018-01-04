b89d5d0bbb273234714b4ceaee473d0834e143d7
xftroxgpx <xftroxgpx@protonmail.com>
2018-01-05 00:48:11 +0100

    fix required: BUILD_DATE should be updated

--- recompile_self/build.rs
+++ recompile_self/build.rs
@@ -17,7 +17,7 @@ fn main() {
 
   //the following code from https://github.com/mitnk/cicada/blob/5fac888ccc3cef0abc24e2d3bdf1655eddfdbc98/src/build.rs and slightly modified:
   extern crate time;
-  use std::process::Command;//1234
+  use std::process::Command;
   match Command::new("git").args(&["rev-parse", "HEAD"]).output() {
       Ok(x) => {
           let git_hash = String::from_utf8_lossy(&x.stdout);
@@ -29,6 +29,6 @@ fn main() {
       }
   }
   let tm = time::now();
-  println!("cargo:rustc-env=BUILD_DATE={}", tm.to_utc().rfc822());
+  println!("cargo:rustc-env=BUILD_DATE={}", tm.to_utc().rfc822()); //FIXME: so this is being cached and depending strictly upon when the last change to build.rs was made! ie. if you touch src/main.rs but not build.rs the BUILD_DATE is the stale cached one from before!
   println!("cargo:warning=Hey, here's a warning from build.rs, for a reason! noting that BUILD_DATE is {}",tm.to_utc().rfc822()); //is a message that will be printed to the main console after a build script has finished running.
 }
