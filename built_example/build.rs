4e6b651dd957b01dc3c0e9ae0ab3630e367e5d65
xftroxgpx <xftroxgpx@protonmail.com>
2018-02-20 11:17:53 +0100

    note why it fails

--- built_example/build.rs
+++ built_example/build.rs
@@ -9,6 +9,6 @@ fn main() {
 //    let opts=built::Options::default() //temporary value does not live long enough
 //        .set_dependencies(true);
     let mut opts=built::Options::default();
-    opts.set_dependencies(true);//FIXME: thread 'main' panicked at 'Failed to acquire build-time information: Os { code: 2, kind: NotFound, message: "No such file or directory" }', libcore/result.rs:945:5
+    opts.set_dependencies(true);//FIXME: thread 'main' panicked at 'Failed to acquire build-time information: Os { code: 2, kind: NotFound, message: "No such file or directory" }', libcore/result.rs:945:5  // this most likely cannot find Cargo.lock file, because we're inside a workspace!
     built::write_built_file_with_opts2(&opts).expect("Failed to acquire build-time information");
 }
