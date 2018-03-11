a23affefa4a07229c5bf3d47803f6d09538cb314
xftroxgpx <xftroxgpx@protonmail.com>
2018-03-12 00:48:27 +0100

    the use keyword

---
+++ rust_books/2_second_edition/ch07-01/use_mod/src/main.rs
@@ -0,0 +1,42 @@
+//src: file:///home/xftroxgpx/build/2nonpkgs/rust.stuff/book/second-edition/book/ch07-03-importing-names-with-use.html#bringing-names-into-scope-with-the-use-keyword
+
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {}
+        }
+    }
+}
+
+use a::series::of;
+
+fn main() {
+    of::nested_modules();
+
+    let _red = Red; //see below (PgDn)
+    let _yellow = Yellow;
+    let _green = TrafficLight::Green;
+    //We’re still specifying the TrafficLight namespace for the Green variant because we didn’t include Green in the use statement.
+}
+//The use keyword brings only what we’ve specified into scope: it does not bring children of modules into scope. That’s why we still have to use of::nested_modules when we want to call the nested_modules function.
+
+//use a::series::of::nested_modules;
+//
+//fn main() {
+//    nested_modules();
+//}
+
+//Because enums also form a sort of namespace like modules, we can bring an enum’s variants into scope with use as well. For any kind of use statement, if you’re bringing multiple items from one namespace into scope, you can list them using curly brackets and commas in the last position, like so:
+
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+use TrafficLight::{Red, Yellow};
+
+//use TrafficLight::*;
+
+//The * will bring into scope all the visible items in the TrafficLight namespace. You should use globs sparingly: they are convenient, but this might also pull in more items than you expected and cause naming conflicts.
+
