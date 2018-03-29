b8ca80aa72bb64228982cdf763bd0de5b87bce10
xftroxgpx <xftroxgpx@protonmail.com>
2018-03-30 01:04:54 +0200

    nevermind, i32 is default - rather than infered(from anything other than another type!)

--- rust_oddities/i32_despide_overflow/src/main.rs
+++ rust_oddities/i32_despide_overflow/src/main.rs
@@ -11,6 +11,9 @@ fn main() {
 
     //this isn't:
     let b=-4000000000;// defaulting to i32 despite the 'literal out of range for i32'
+    //Actually, nevermind, since this behaviour is documented as such: "Rust defaults to an i32, which is the type of secret_number unless you add type information elsewhere that would cause Rust to infer a different numerical type."
+    //see https://github.com/rust-lang/rust/issues/47739#issuecomment-377392401
+    //let b=-4000000000_i64;// now properly inferred as i64
     print_type_of(&b);// i32
     println!("{}",b);// 294967296
 }
