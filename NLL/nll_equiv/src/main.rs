d1d08db898a1aa24eccb1bc7b68f40754d4ce058
xftroxgpx <xftroxgpx@protonmail.com>
2019-01-02 16:54:44 +0100

    feature nll in code, so cargo clippy works

--- NLL/nll_equiv/src/main.rs
+++ NLL/nll_equiv/src/main.rs
@@ -1,4 +1,6 @@
-//#![feature(nll)] //only needed when not passing: -Z nll or -Z borrowck=mir
+#![feature(nll)] //only needed when not passing: -Z nll or -Z borrowck=mir
+//actually "-Z nll" is no longer a thing! but the mir one works!
+//must allow the feature in code here, or how else am I gonna run `cargo clippy`?
 
 fn main() {}
 
