1a3550c486f9c5c88d62d38b27bd7a376b9c78be
howaboutsynergy <howaboutsynergy@pm.me>
2019-06-03 02:59:21 +0200

    the messy way

--- book/l_18_27/src/main.rs
+++ book/l_18_27/src/main.rs
@@ -4,8 +4,19 @@ fn main() {
     let y = 5;
 
     match x {
-        Some(50) => println!("Got 50"),
+        Some(50) => { println!("Got 50") } // when braces comma not needed?!!
         Some(n) if n == y => println!("Matched, n = {:?}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    match x {
+        Some(n) => {
+            if 50 == n { println!("Got 50")
+            }
+            if n == y {
+                println!("Matched, n = {:?}", n)
+            }
+        } // when braces comma not needed?!!
         _ => println!("Default case, x = {:?}", x),
     }
 
