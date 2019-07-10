07074e3db0ba1012492c57e6a8c4e3c21ff93eca
howaboutsynergy <howaboutsynergy@pm.me>
2019-07-10 02:41:20 +0200

    ok, this is uniq id for each thread
    not for each call

--- getuniqid2/src/main.rs
+++ getuniqid2/src/main.rs
@@ -2,11 +2,14 @@ use std::thread;
 use thread_id;
 
 fn main() {
+    //ok so same number, per thread! not new uniq number on each call! got it!
     let spawned = thread::spawn(move || {
+        println!("spawned thread has id {}", thread_id::get());
         println!("spawned thread has id {}", thread_id::get());
     });
     //spawned.join().unwrap();
 
+    println!("main thread has id {}", thread_id::get());
     println!("main thread has id {}", thread_id::get());
     spawned.join().unwrap();
 }
