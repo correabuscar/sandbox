b116a8c1258de62788f1623995274b7d098fd588
howaboutsynergy <howaboutsynergy@pm.me>
2019-07-12 19:58:50 +0200

    add SIGINT and make optional = false explicit

--- others/sighooksample1/src/main.rs
+++ others/sighooksample1/src/main.rs
@@ -4,18 +4,19 @@ extern crate signal_hook;
 use std::io::Error;
 use std::thread;
 
-use signal_hook::iterator::Signals;
+use signal_hook::{iterator::Signals, SIGINT};
 
 fn main() -> Result<(), Error> {
     let signals = Signals::new(&[
         signal_hook::SIGUSR1,
         signal_hook::SIGUSR2,
         signal_hook::SIGHUP,
+        SIGINT,
     ])?;
     thread::spawn(move || {
         for signal in signals.forever() {
             match signal {
-                signal_hook::SIGUSR1 => {
+                signal_hook::SIGUSR1 | SIGINT => {
                     println!("exiting");
                     break;
                 }
