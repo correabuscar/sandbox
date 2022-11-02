2380fab502516b95467032098aef8e0bca75479f
correabuscar <correabuscar+githubcommits@gmail.com>
2022-11-03 00:55:50 +0100

    update rust helper scripts and no implicit prelude

--- rust/rust_book/ch02_00_ggt/src/main.rs
+++ rust/rust_book/ch02_00_ggt/src/main.rs
@@ -1,5 +1,10 @@
-use std::io;
+#![no_implicit_prelude]
+
+// "::" required, see issue: https://github.com/rust-lang/rust/issues/56390
+use ::std::io;
 //use std::io as io;
+use ::std::println;
+use ::std::string::String;
 
 fn main() {
     println!("Guess the number!");
--- rust/rust_book/helper_scripts/travis_script.sh
+++ rust/rust_book/helper_scripts/travis_script.sh
@@ -4,8 +4,11 @@
 #^ don't need this during compiling! would only need it on run (no tests use it currently either)
 
 set -ex
+time {
 echo '!! Building...'
 #time cargo build --verbose --all
-time cargo clippy --all-targets --all-features --verbose --all
+#time cargo clippy --all-targets --all-features --verbose --all
+time cargo clippy --all-targets --verbose --all
 echo '!! Testing...'
 time cargo test --verbose --all
+}
