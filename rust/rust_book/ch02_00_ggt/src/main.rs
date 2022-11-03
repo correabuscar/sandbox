3696429f54b965a3606046dc33b9cdfb29011900
correabuscar <correabuscar+githubcommits@gmail.com>
2022-11-03 05:06:00 +0100

    manage without use-ing 'trait's

--- rust/rust_book/ch02_00_ggt/src/main.rs
+++ rust/rust_book/ch02_00_ggt/src/main.rs
@@ -25,6 +25,10 @@
 
 extern crate alloc; //or else: error[E0433]: failed to resolve: could not find `alloc` in the list of imported crates
 
+//use ::rand::Rng; //trait
+
+//use ::std::iter::Iterator; // for .rev()
+
 // "::" required, see issue: https://github.com/rust-lang/rust/issues/56390
 //use ::std::io;
 //use ::std::io as io;
@@ -70,6 +74,17 @@ fn main() {
     ::std::println!("a_after = {a:?}");
     //::std::println!("b_after = {b}"); //can't , moved above!
 
+    ::std::println!("Guess the number!");
+    //let secret_number = ::rand::thread_rng().gen_range(1..=100); // needs  use ::rand::Rng; //trait
+
+    let secret_number = ::rand::Rng::gen_range(&mut ::rand::thread_rng(), 1..=100); // this doesn't! src: https://stackoverflow.com/a/25274740/19999437
+
+    //tmp allow reversed range (which appears to be taken as empty range)
+    //#[allow(clippy::reversed_empty_ranges)]
+    //let secret_number = ::rand::Rng::gen_range(&mut ::rand::thread_rng(), 100..=1); // thread 'main' panicked at 'cannot sample empty range',
+    //let secret_number = ::rand::Rng::gen_range(&mut ::rand::thread_rng(), (100..=1).rev()); // the trait `SampleRange<_>` is not implemented for `Rev<RangeInclusive<{integer}>>`
+
+    ::std::println!("The secret number is: {secret_number}");
     ::std::println!("Please input your guess.");
 
     let mut guess = ::alloc::string::String::new();
