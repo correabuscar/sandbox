ca8fad88acd0ae05e8732af0a4756bdb50fcbb89
correabuscar <correabuscar+githubcommits@gmail.com>
2022-11-08 13:18:06 +0100

    blog_os: make it work in virtualbox
    add HLT instructions but they've no effect!
    unknown why yet, see:
    https://github.com/phil-opp/blog_os/issues/528

--- rust/writing_an_OS_in_Rust/blog_os/src/main.rs
+++ rust/writing_an_OS_in_Rust/blog_os/src/main.rs
@@ -19,13 +19,21 @@ pub extern "C" fn _start() -> ! {
 
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    loop {}
+    x86_64::instructions::interrupts::disable();
+    loop {
+        //x86_64::instructions::hlt();
+        x86_64::instructions::interrupts::enable_and_hlt();
+    }
 }
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    x86_64::instructions::interrupts::disable();
+    loop {
+        //x86_64::instructions::hlt();
+        x86_64::instructions::interrupts::enable_and_hlt();
+    }
 }
 
 
