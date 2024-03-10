5a0a01380d7f1fbfcb65d198fcff9c7b649c8fac
correabuscar <correabuscar+githubcommits@gmail.com>
2024-03-10 17:09:07 +0100

    add example

--- git/wdiff/buggedown2.bash
+++ git/wdiff/buggedown2.bash
@@ -4,4 +4,4 @@ regex='[[:alnum:]]+'
 regex='.'
 #regex='[[:space:]]+|[[:alnum:]]+|[^[:alnum:][:space:]]|[[:space:]]'
 git diff -U1 --no-index --word-diff=plain --word-diff-regex="$regex" --color=always -- <(echo -e "a b c\nNEWLINE1\nNEWLINE2\nENDLINE") <(echo -e "a F c\n\nENDLINE") | cat 
-#FIXME: the 'added line1' shows concatenated to prev. line!! but it should be on next line alone!
+#FIXME: the '[-NEWLINE1-]' diff shows concatenated to prev. line!! but it should be on next line alone!
