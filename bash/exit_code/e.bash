579af259f871bd0937abbfbfea7c42c1f21596ea
correabuscar <correabuscar+githubcommits@gmail.com>
2024-03-11 15:46:08 +0100

    hmm can still use PIPESTATUS to get the exit code

---
+++ bash/exit_code/e.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail #like the original: https://github.com/pasadoorian/displaygoat/blob/1883636decae12e8a90da5344798a096c673427c/displaygoat.sh#L19
+OUTPUT="/tmp/some${RANDOM}.log"
+SHOW="dummy"
+
+#re https://github.com/pasadoorian/displaygoat/issues/3
+#
+set -x
+#false || : #use 'false' to assume something returned bad exit code (aka 1) // "# Its okay if this fails, so we append the "always true" e.g. !! :
+
+if ! /bin/false; then
+  #ret="$?" #can't get that exit code here, so this will be '0' due to the 'if .. then' itself!
+  ret="${PIPESTATUS[@]}" #but can still get it via PIPESTATUS array for this `if ! command; then` case!
+  echo "feh command failed, this is a big deal" | tee $OUTPUT
+  exit $ret  #note though, that in the original he doesn't want to exit because "it's no big deal" ?) we exit anyway I guess.
+else
+  echo "Success: displaying the images for $SHOW" | tee $OUTPUT
+fi
