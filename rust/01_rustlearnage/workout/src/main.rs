b222eb89c6ec102334c6d53b7845890f171fbc18
correabuscar <correabuscar+githubcommits@gmail.com>
2024-03-19 11:22:23 +0100

    updates for security

--- rust/01_rustlearnage/workout/src/main.rs
+++ rust/01_rustlearnage/workout/src/main.rs
@@ -1,13 +1,14 @@
 extern crate cursive;
 
-use cursive::Cursive;
+//use cursive::Cursive;
 use cursive::views::{Dialog, TextView};
 
 fn main() {
 	println!("Hello, world!");
     // src: https://github.com/gyscos/Cursive
 	// Creates the cursive root - required for every application.
-	let mut siv = Cursive::new();
+	//let mut siv = Cursive::new();
+	let mut siv = cursive::default();
 
 	// Creates a dialog with a single "Quit" button
 	siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
