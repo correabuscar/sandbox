1385e291654554a3c3a29b25c1981e68aad2a7c2
correabuscar <correabuscar+githubcommits@gmail.com>
2024-03-23 20:34:02 +0100

    attempt to catch SIGABRT from call - fail

--- rust/02_reflo/cli-wg/signals/src/main.rs
+++ rust/02_reflo/cli-wg/signals/src/main.rs
@@ -1,11 +1,13 @@
 // file:///home/user/build/2nonpkgs/rust.stuff/cli-wg/book/in-depth/signals.html
-use signal_hook::{iterator::Signals, SIGINT};
+use signal_hook::{iterator::Signals, SIGABRT, SIGINT};
 use std::{error::Error, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let signals = Signals::new(&[SIGINT])?;
+    let signals = Signals::new(&[
+        SIGINT, SIGABRT, // XXX: can catch this from `kill -6 $pid` tho.
+    ])?;
 
-    thread::spawn(move || {
+    let first = thread::spawn(move || {
         for sig in signals.forever() {
             println!("Received signal {:?}", sig);
             if sig == 2 {
@@ -13,9 +15,14 @@ fn main() -> Result<(), Box<dyn Error>> {
                 break;
             }
         }
-    })
-    .join()
-    .unwrap();
+    });
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    let second = thread::spawn(move || {
+        //std::process::abort(); //can't catch this TODO:
+    });
+    println!("{:#?}", first.join().unwrap());
+    println!("{:#?}", second.join().unwrap());
+    println!("Main says bye.");
 
     Ok(())
 }
