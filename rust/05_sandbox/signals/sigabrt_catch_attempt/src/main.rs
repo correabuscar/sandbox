95e15b409a4e9b1d6f6f3befeb81afa18c5fd255
correabuscar <correabuscar+githubcommits@gmail.com>
2024-03-24 14:55:26 +0100

    add panic handler

--- rust/05_sandbox/signals/sigabrt_catch_attempt/src/main.rs
+++ rust/05_sandbox/signals/sigabrt_catch_attempt/src/main.rs
@@ -19,7 +19,7 @@ extern "C" fn handle_abort(signal: libc::c_int) {
 
     // Add your custom handling logic here
     let inst = MyStruct;
-    panic!("from within my custom abort handler");
+    panic!("panic from within my custom abort handler");
     //panic!("from within my custom abort handler {}", inst); // infinite recursion panic
 
     // Terminate the process
@@ -27,12 +27,59 @@ extern "C" fn handle_abort(signal: libc::c_int) {
 }
 
 
+fn set_panic_hook_too() {
+    use std::cell::RefCell;
+    std::panic::set_hook(Box::new(move |panic_info| {
+        println!("Custom panic handler starting!");
+        thread_local! {
+            static CALLED_ONCE: RefCell<bool> = RefCell::new(false);
+        }
+        let double_panic=
+        // Check if CALLED_ONCE is true, if yes, abort, otherwise set it to true
+            CALLED_ONCE.with(|called_once| {
+            let mut called_once = called_once.borrow_mut();
+            if *called_once {
+                true
+            } else {
+                *called_once = true;
+                false
+            }
+        });
+
+        let double_text=
+            if double_panic {
+                "double "
+            } else {
+                ""
+            };
+        // Print the panic message
+        println!("Custom panic handler processing payload");
+        if let Some(message) = panic_info.payload().downcast_ref::<&str>() {
+            println!("Custom panic handler caught {}panic: {}", double_text,message);
+        } else {
+            println!("Custom panic handler caught {}panic",double_text);
+        }
+
+        // Print a backtrace if available
+        if let Some(location) = panic_info.location() {
+            println!("Panic occurred in file '{}' at line {}", location.file(), location.line());
+            println!("{}", std::backtrace::Backtrace::capture());
+        }
+        if double_panic {
+            println!("Aborting the process due to double panic detected...");
+            println!("{}", std::backtrace::Backtrace::capture());
+        }
+        println!("Custom panic handler finished.");
+    }));
+}
 
 fn main() {
     // Install a signal handler for SIGABRT
     unsafe {
         libc::signal(libc::SIGABRT, handle_abort as usize);
     }
+
+    set_panic_hook_too();
 
     // Trigger an abort
     println!("Triggering abort");
