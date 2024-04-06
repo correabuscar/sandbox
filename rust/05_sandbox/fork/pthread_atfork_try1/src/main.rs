de94be08f4400844d2c6e47d0a16b8212f46f449
correabuscar <correabuscar+githubcommits@gmail.com>
2024-04-06 21:55:38 +0200

    handle the case where OS has no fork()

--- rust/05_sandbox/fork/pthread_atfork_try1/src/main.rs
+++ rust/05_sandbox/fork/pthread_atfork_try1/src/main.rs
@@ -12,7 +12,12 @@ fn main() {
 //    // Shared state for boolean flags
 //    let fork_flags = Arc::new(Mutex::new(ForkFlags::default()));
 
+    #[cfg(not(unix))]
+    eprintln!("There's no fork() on your OS: {}", std::env::consts::OS);
+    #[cfg(unix)]
+    eprintln!("Welcome. There's fork() on this OS: {}", std::env::consts::OS);
     // Register fork handlers
+    #[cfg(unix)]
     unsafe {
         let result: libc::c_int = libc::pthread_atfork(
             Some(prepare),
@@ -48,6 +53,8 @@ fn main() {
 //    } else {
 //        println!("No fork occurred in the child process.");
 //    }
+//    XXX: fork is part of 'unix' cfg: https://github.com/rust-lang/libc/blob/a0f5b4b21391252fe38b2df9310dc65e37b07d9f/src/lib.rs#L92C5-L97C25
+    #[cfg(unix)]
     unsafe {
         libc::fork();
         //libc::vfork(); // doesn't use the hooks from pthread_atfork()
@@ -59,20 +66,27 @@ fn main() {
     };
 }
 
+//cfg_if! {
+//if #[cfg(unix)] {
 // Fork handlers
+#[cfg(unix)]
 unsafe extern "C" fn prepare() {
     // You can perform any necessary actions before fork() in the parent process
     eprintln!("!! prepare pid={}",std::process::id());
 }
 
+#[cfg(unix)]
 unsafe extern "C" fn parent() {
     // You can perform any necessary actions after fork() in the parent process
     eprintln!("!! parent pid={}",std::process::id());
 }
 
+#[cfg(unix)]
 unsafe extern "C" fn child() {
     // You can perform any necessary actions after fork() in the child process
     std::thread::sleep(std::time::Duration::from_secs(1));
     eprintln!("!! child pid={}",std::process::id());
 }
+//}
+//} //#cfg
 
