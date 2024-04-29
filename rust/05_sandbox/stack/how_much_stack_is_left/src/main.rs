6cd5e776f63f937d63a26f88e4f1ed50d30dd2d0
correabuscar <correabuscar+githubcommits@gmail.com>
2024-04-29 17:34:22 +0200

    this uses even more stack, odd

--- rust/05_sandbox/stack/how_much_stack_is_left/src/main.rs
+++ rust/05_sandbox/stack/how_much_stack_is_left/src/main.rs
@@ -1,14 +1,36 @@
 #![feature(thread_local)]
 
+use std::io::Write;
+
 // Function to recursively call itself
 fn recurse(depth: usize) {
     // Print the current depth to observe stack usage
-    println!("Depth: {}, stack left: {:?}", depth, stacker::remaining_stack());
+    eprintln!("Depth: {}, stack left: {:?}", depth, stacker::remaining_stack());
     let cloj=|| {
-        //XXX: so array allocs stack mem in multiples of 16?
+        //XXX: so array allocs stack mem in multiples of 16? presumably it's padding.
         let mut _buffer = [0u8; 16*10];//960 left for 16 or less, but 944 left for 17 hmmm
         //816 left for 16*10, and 16*10+1 is overflow! so 816 or less, or maybe <=800 is overflow!
-        println!("From cloj, stack left: {:?}", stacker::remaining_stack());
+        println!("From cloj, stack left: {:?}", stacker::remaining_stack());//this is ok with 16*10
+        //eprintln!("From cloj, stack left: {:?}", stacker::remaining_stack());//this blows stack,
+                                                                             //needs 16*8
+//        // Get the standard output stream
+//        let stdout = std::io::stderr();
+//        // Lock the stdout so that it can be written to
+//        let mut handle = stdout.lock();
+//
+//        // Evaluate the expression and store the result in a variable
+//        let remaining_stack = stacker::remaining_stack().unwrap();
+//
+//        // Convert the value to a string representation
+//        let stack_str = remaining_stack.to_string();
+//
+//        // Write the string to stdout
+//        let _ =handle.write_all(b"From cloj, stack left: ");
+//        let _ =handle.write_all(stack_str.as_bytes());
+//        let _=handle.write_all(b"\n");
+//
+//        // Ensure the output is flushed to the console
+//        handle.flush().expect("Failed to flush stdout");
     };
 
     // Recursively call itself
@@ -23,7 +45,7 @@ fn recurse(depth: usize) {
 }
 
 fn main() {
-    println!("Hello, main world! {:?}", stacker::remaining_stack());
+    eprintln!("Hello, main world! {:?}", stacker::remaining_stack());
     // Set the desired stack size
     let stack_size = 150*128+5696;//via `cargo run` to 150 depth, 149 is min.
 
