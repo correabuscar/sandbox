fbc4c3b9923304d3d5437fdbcd2ae044c69bc1ff
correabuscar <correabuscar+githubcommits@gmail.com>
2024-04-30 11:10:22 +0200

    update example for security fixes

--- rust/01_rustlearnage/glium/tutorial-05/src/main.rs
+++ rust/01_rustlearnage/glium/tutorial-05/src/main.rs
@@ -1,39 +1,40 @@
 #[macro_use]
 extern crate glium;
+use glium::Surface;
 
 fn main() {
-    use glium::{glutin, Surface};
-
-    let mut events_loop = glutin::EventsLoop::new();
-    let window = glutin::WindowBuilder::new();
-    let context = glutin::ContextBuilder::new();
-    let display = glium::Display::new(window, context, &events_loop).unwrap();
+    let event_loop = winit::event_loop::EventLoopBuilder::new()
+        .build()
+        .expect("event loop building");
+    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
+        .with_title("Glium tutorial #5")
+        .build(&event_loop);
 
     #[derive(Copy, Clone)]
     struct Vertex {
         position: [f32; 2],
+        color: [f32; 3],
     }
-
-    implement_vertex!(Vertex, position);
-
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    implement_vertex!(Vertex, position, color);
+    let shape = vec![
+        Vertex { position: [-0.5, -0.5], color: [1.0, 0.0, 0.0] },
+        Vertex { position: [ 0.0,  0.5], color: [0.0, 1.0, 0.0] },
+        Vertex { position: [ 0.5, -0.25], color: [0.0, 0.0, 1.0] }
+    ];
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
 
     let vertex_shader_src = r#"
         #version 140
 
         in vec2 position;
-        out vec2 my_attr;      // our new attribute
+        in vec3 color;      // our new attribute
+        out vec3 vertex_color;
 
         uniform mat4 matrix;
 
         void main() {
-            my_attr = position;     // we need to set the value of each `out` variable.
+            vertex_color = color; // we need to set the value of each `out` variable.
             gl_Position = matrix * vec4(position, 0.0, 1.0);
         }
     "#;
@@ -41,49 +42,60 @@ fn main() {
     let fragment_shader_src = r#"
         #version 140
 
-        in vec2 my_attr;
+        in vec3 vertex_color;
         out vec4 color;
 
         void main() {
-            color = vec4(my_attr, 0.0, 1.0);   // we build a vec4 from a vec2 and two floats
+            color = vec4(vertex_color, 1.0);   // We need an alpha value as well
         }
     "#;
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    let mut t = -0.5;
-    let mut closed = false;
-    while !closed {
-        // we update `t`
-        t += 0.0002;
-        if t > 0.5 {
-            t = -0.5;
-        }
+    let mut t:f32 = -0.5;
 
-        let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+    event_loop.run(move |ev, window_target| {
+        match ev {
+            winit::event::Event::WindowEvent { event, .. } => match event {
+                winit::event::WindowEvent::CloseRequested => {
+                    window_target.exit();
+                },
+                // We now need to render everyting in response to a RedrawRequested event due to the animation
+                winit::event::WindowEvent::RedrawRequested => {
+                    // we update `t`
+                    t += 0.02;
+                    let x = t.sin() * 0.5;
 
-        let uniforms = uniform! {
-            matrix: [
-                [1.0, 0.0, 0.0, 0.0],
-                [0.0, 1.0, 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [ t , 0.0, 0.0, 1.0f32],
-            ]
-        };
+                    let mut target = display.draw();
+                    target.clear_color(0.0, 0.0, 1.0, 1.0);
 
-        target.draw(&vertex_buffer, &indices, &program, &uniforms,
-                    &Default::default()).unwrap();
-        target.finish().unwrap();
+                    let uniforms = uniform! {
+                        matrix: [
+                            [1.0, 0.0, 0.0, 0.0],
+                            [0.0, 1.0, 0.0, 0.0],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [  x, 0.0, 0.0, 1.0f32],
+                        ]
+                    };
 
-        events_loop.poll_events(|event| {
-            match event {
-                glutin::Event::WindowEvent { event, .. } => match event {
-                    glutin::WindowEvent::CloseRequested => closed = true,
-                    _ => ()
+                    target.draw(&vertex_buffer, &indices, &program, &uniforms,
+                                &Default::default()).unwrap();
+                    target.finish().unwrap();
+                },
+                // Because glium doesn't know about windows we need to resize the display
+                // when the window's size has changed.
+                winit::event::WindowEvent::Resized(window_size) => {
+                    display.resize(window_size.into());
                 },
                 _ => (),
-            }
-        });
-    }
+            },
+            // By requesting a redraw in response to a AboutToWait event we get continuous rendering.
+            // For applications that only change due to user input you could remove this handler.
+            winit::event::Event::AboutToWait => {
+                window.request_redraw();
+            },
+            _ => (),
+        }
+    })
+    .unwrap();
 }
