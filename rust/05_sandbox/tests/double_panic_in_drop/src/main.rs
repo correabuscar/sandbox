4344f860cd90504e55d2ee921c127624f7300485
correabuscar <correabuscar+githubcommits@gmail.com>
2024-05-03 09:59:18 +0200

    double panic in drop, seems ok in tests

--- rust/05_sandbox/tests/double_panic_in_drop/src/main.rs
+++ rust/05_sandbox/tests/double_panic_in_drop/src/main.rs
@@ -4,7 +4,9 @@ struct Double;
 impl Drop for Double {
     fn drop(&mut self) {
         // 2 panics are active at once, but this is fine since it is caught.
-        let _ =std::panic::catch_unwind(|| panic!("twice"));
+        //let _ =std::panic::catch_unwind(|| panic!("twice"));
+        //what if it's not caught?! double panic all the way
+        let _ = panic!("twice");
     }
 }
 
