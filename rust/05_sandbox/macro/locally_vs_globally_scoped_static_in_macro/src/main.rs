af64463c19d16940f5deb7f6426ea38460c940bc
correabuscar <correabuscar+githubcommits@gmail.com>
2024-05-06 09:12:45 +0200

    add comments

--- rust/05_sandbox/macro/locally_vs_globally_scoped_static_in_macro/src/main.rs
+++ rust/05_sandbox/macro/locally_vs_globally_scoped_static_in_macro/src/main.rs
@@ -2,6 +2,11 @@ macro_rules! increment_static {
     (local) => {
         {
             static mut COUNTER: i32 = 0;
+            /* "When you define a static variable within a macro, Rust ensures that each invocation
+             * of the macro generates a unique static variable by mangling the name of the static.
+             * This mangling process ensures that each static variable has a distinct name,
+             * allowing multiple invocations of the macro to coexist without conflicts." - chatgpt 3.5
+             */
             unsafe {
                 COUNTER += 1;
                 println!("Static counter: {}", COUNTER);
@@ -10,6 +15,7 @@ macro_rules! increment_static {
     };
     (global) => {
         {
+            /*can't call this macro more than once due to the no_mangle*/
             #[no_mangle]
             static mut COUNTER: i32 = 0;
             unsafe {
