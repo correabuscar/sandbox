82fa338174cf988345b64ca5f381a56b573d0fef
correabuscar <correabuscar+githubcommits@gmail.com>
2024-05-09 21:48:39 +0200

    runtime borrow checking for custom hashset
    allows removal of other elements while holding ref to a diff. one
    which a normal hashset wouldn't allow due to being tied to the whole set

---
+++ rust/05_sandbox/hash/hashset/runtime_borrow_checks/src/main.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::hash::Hash;
+
+struct CustomHashSet<T> {
+    elements: HashMap<T, Rc<T>>,
+}
+
+impl<T: Clone + Eq + Hash> CustomHashSet<T> {
+    fn new() -> Self {
+        Self {
+            elements: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, value: T) {
+        let cloned_value = value.clone();
+        self.elements.insert(value, Rc::new(cloned_value));
+    }
+
+    fn get(&self, key: &T) -> Option<Rc<T>> {
+        self.elements.get(key).cloned()
+    }
+
+    fn remove(&mut self, key: &T) {
+        self.elements.remove(key);
+    }
+}
+
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct MyType<T>(T);
+
+fn main() {
+    let mut set = CustomHashSet::new();
+    set.insert(MyType(42));
+    set.insert(MyType(43));
+
+    let elem_ref = set.get(&MyType(42));
+    //XXX: can remove an element while holding a ref to another elem.
+    println!("{:?}", set.get(&MyType(43)));
+    set.remove(&MyType(43));
+    println!("{:?}", set.get(&MyType(43)));
+
+    println!("{:?}", elem_ref); // Ensure elem_ref is used
+}
