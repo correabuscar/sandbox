607e95deacaf28e88c356042403135fc5d729c77
correabuscar <correabuscar+githubcommits@gmail.com>
2024-05-09 20:56:12 +0200

    ok so this runtime borrow tracking might work
    but maybe it can be done with compile-time borrow checking(next try)

--- rust/05_sandbox/recursion/maybeuninit_refcell_drop_while_borrowed_dont_give_refcell/src/main.rs
+++ rust/05_sandbox/recursion/maybeuninit_refcell_drop_while_borrowed_dont_give_refcell/src/main.rs
@@ -2,7 +2,7 @@
 
 use std::cell::{RefCell,Ref, RefMut};
 use std::mem::MaybeUninit;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicBool, Ordering, AtomicUsize};
 use std::borrow::BorrowMut;
 use std::borrow::Borrow;
 
@@ -10,6 +10,23 @@ struct Foo<const N:usize, T> {
     is_about_to_be_set:[AtomicBool;N],
     values:[MaybeUninit<RefCell<T>>;N],
     is_set:[AtomicBool;N],
+    active_borrows: [AtomicUsize;N],
+}
+
+
+#[derive(Debug)]
+struct BorrowGuard<'a, T> {
+    //foo: &'a Foo<N, T>,
+    value_for_this_thread: RefMut<'a, T>,
+    active_borrows_count_for_this_thread: &'a AtomicUsize,
+}
+
+impl<'a, T> Drop for BorrowGuard<'a, T> {
+    fn drop(&mut self) {
+        // Decrement the active borrows count when the guard is dropped
+        //self.foo.active_borrows.fetch_sub(1, Ordering::Release);
+        self.active_borrows_count_for_this_thread.fetch_sub(1, Ordering::Release);
+    }
 }
 
 impl<const N:usize, T> Foo<N,T> {
@@ -21,14 +38,17 @@ impl<const N:usize, T> Foo<N,T> {
             index+=1;
         }
         const CONST_INIT:AtomicBool=AtomicBool::new(false);
+        const CONST_INIT2:AtomicUsize=AtomicUsize::new(0);//no borrows initially
         Self {
             is_about_to_be_set:[CONST_INIT;N],
             values,
             is_set:[CONST_INIT;N],
+            active_borrows:[CONST_INIT2;N],
         }
     }
 
-    fn try_get_or_set<'a>(&'a self, value:T) -> Option<RefMut<'a,T>> {
+    //fn try_get_or_set<'a>(&'a self, value:T) -> Option<RefMut<'a,T>> {
+    fn try_get_or_set<'a>(&'a self, value:T) -> Option<BorrowGuard<'a,T>> {
         let index=N-1;
         #[allow(unused_comparisons)]
         {
@@ -71,10 +91,17 @@ impl<const N:usize, T> Foo<N,T> {
         //let refcell=unsafe { self.values[index].assume_init_ref() };
         let ref_to_refcell=unsafe { self.values[index].assume_init_ref() };
         //return Some(unsafe { self.values[index].assume_init_ref() });
-        return Some(ref_to_refcell.borrow_mut());
+        //self.active_borrows[index].fetch_add(1, Ordering::Acquire);
+        let active_borrows_count_for_this_thread:&AtomicUsize=&self.active_borrows[index];
+        active_borrows_count_for_this_thread.fetch_add(1, Ordering::Acquire);
+        let borrow_guard=BorrowGuard {
+            value_for_this_thread:ref_to_refcell.borrow_mut(),//FIXME: should can be mut or non-mut
+            active_borrows_count_for_this_thread
+        };
+        return Some(borrow_guard);
     }//fn
 
-    fn drop_elem(&self) {
+    fn try_drop_elem(&self) -> Result<(), &'static str> {
         let index=N-1;
         #[allow(unused_comparisons)]
         {
@@ -84,6 +111,12 @@ impl<const N:usize, T> Foo<N,T> {
         if !self.is_set[index].load(Ordering::Acquire) {
             panic!("Bad call");
         }
+        let active_borrows=self.active_borrows[index].load(Ordering::Acquire);
+        assert!(active_borrows>=0);
+        if active_borrows > 0 {
+            return Err("can't drop while active borrows");
+        }
+        assert_eq!(active_borrows,0);
         //ok so it's set here:
         //step1of3:
         match self.is_set[index].compare_exchange(true, false, Ordering::Release, Ordering::Acquire) {
@@ -117,13 +150,14 @@ impl<const N:usize, T> Foo<N,T> {
                 panic!("No drop, some other thread dropped it first!");
             }
         }//match
+        Ok(())
     }
 }//impl
 
 #[derive(Debug)]
 struct MyType(i32);
 
-fn main() {
+fn main() -> Result<(),&'static str> {
     println!("Hello, world!");
     let inst:Foo<10,MyType>=Foo::new();
     let my=MyType(100);
@@ -131,10 +165,11 @@ fn main() {
     println!("Got {:?}", ref_to_refcell);
     let foo_w=ref_to_refcell.borrow_mut();//no panic, which is ok!
     //let foo_r=ref_to_refcell.borrow();//no panic, which is ok!
-    foo_w.0=1;//panic, which is good
+    foo_w.value_for_this_thread.0=1;//panic, which is good
     println!("direct access={:?}", foo_w);
     println!("Still got {:?}", ref_to_refcell);
-    inst.drop_elem();//FIXME: I shouldn't be able to call this while still having outstanding borrows(ie. given out)
+    //drop(ref_to_refcell);
+    inst.try_drop_elem()?;//FIXME: I shouldn't be able to call this while still having outstanding borrows(ie. given out)
     println!("after dropStill got {:?}", ref_to_refcell);
     let my2=MyType(200);
     let ref_to_refcell2=inst.try_get_or_set(my2).unwrap();//panics, which is good but not enough
@@ -156,4 +191,5 @@ fn main() {
 
     println!("Still got {:?}", ref_to_refcell);
     println!("Still Got2 {:?}", ref_to_refcell2);
+    Ok(())
 }
