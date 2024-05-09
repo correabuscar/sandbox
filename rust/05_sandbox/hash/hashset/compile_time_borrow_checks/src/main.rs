7ccf92758d95007342ac3026646ee9a4c8c9dcba
correabuscar <correabuscar+githubcommits@gmail.com>
2024-05-09 21:59:39 +0200

    trick?

---
+++ rust/05_sandbox/hash/hashset/compile_time_borrow_checks/src/main.rs
@@ -0,0 +1,69 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::marker::PhantomData;
+
+#[derive(Eq, Hash, PartialEq, Debug)]
+struct MyType(i32); // Wrapper type for i32
+
+// Custom smart pointer type with compile-time borrow checking
+struct CustomRef<'a, T> {
+    data: &'a T,
+    _marker: PhantomData<&'a T>, // PhantomData to indicate borrowing relationship
+}
+
+impl<'a, T> CustomRef<'a, T> {
+    fn new(data: &'a T) -> Self {
+        Self {
+            data,
+            _marker: PhantomData,
+        }
+    }
+
+    // Add methods for accessing and modifying the data
+    fn get(&self) -> &'a T {
+        self.data
+    }
+}
+
+// Custom hash set with compile-time borrow checking
+struct CustomHashSet<'a, T: 'a> {
+    elements: HashMap<MyType, &'a T>, // Store references to values
+}
+
+impl<'a, T: 'a + Eq + Hash> CustomHashSet<'a, T> {
+    fn new() -> Self {
+        Self {
+            elements: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, key: MyType, value: &'a T) {
+        self.elements.insert(key, value);
+    }
+
+    fn get(&self, key: &MyType) -> Option<CustomRef<'a, T>> {
+        if let Some(value) = self.elements.get(key) {
+            Some(CustomRef::new(value))
+        } else {
+            None
+        }
+    }
+
+    fn remove(&mut self, key: &MyType) {
+        self.elements.remove(key);
+    }
+}
+
+fn main() {
+    let mut set = CustomHashSet::new();
+    let value1 = MyType(42); // Using MyType wrapper for i32
+    let value2 = MyType(43); // Using MyType wrapper for i32
+    set.insert(value1, &42);
+    set.insert(value2, &43);
+
+    let elem_ref = set.get(&MyType(42)).unwrap(); // Using MyType wrapper for i32
+    set.remove(&MyType(43)); // Using MyType wrapper for i32
+
+    println!("{:?}", elem_ref.get()); // Ensure elem_ref is used
+}
+
