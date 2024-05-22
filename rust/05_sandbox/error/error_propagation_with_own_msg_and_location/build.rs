f4d0525503173c047241921861a88c0a5edf1827
correabuscar <correabuscar+githubcommits@gmail.com>
2024-05-22 19:41:16 +0200

    cargo fmt and some format fixes

--- rust/05_sandbox/error/error_propagation_with_own_msg_and_location/build.rs
+++ rust/05_sandbox/error/error_propagation_with_own_msg_and_location/build.rs
@@ -4,11 +4,16 @@ use std::io::Write;
 use std::path::Path;
 
 fn main() {
-    let current_dir= env::current_dir().unwrap();//needed else temp value dropped compiler error!
+    let current_dir = env::current_dir().unwrap(); //needed else temp value dropped compiler error!
     let current_dir_str = current_dir.to_str().unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("project_dir.rs");
     let mut file = File::create(&dest_path).unwrap();
 
-    writeln!(&mut file, "pub const PROJECT_DIR: &str = \"{}/\";", current_dir_str).unwrap();
+    writeln!(
+        &mut file,
+        "pub const PROJECT_DIR: &str = \"{}/\";",
+        current_dir_str
+    )
+    .unwrap();
 }
--- rust/05_sandbox/error/error_propagation_with_own_msg_and_location/src/main.rs
+++ rust/05_sandbox/error/error_propagation_with_own_msg_and_location/src/main.rs
@@ -1,96 +1,98 @@
 //#![feature(stmt_expr_attributes)]
 //#![deny(unused_must_use)] // yeah it works here, ofc! but it's too broad!
 
-
-
-
 mod my_error_things {
-include!(concat!(env!("OUT_DIR"), "/project_dir.rs")); //gets me 'PROJECT_DIR'
+    include!(concat!(env!("OUT_DIR"), "/project_dir.rs")); //gets me 'PROJECT_DIR'
 
-//const CUSTOM_ERROR_MSG_BUFFER_SIZE: usize = 6;
-pub const CUSTOM_ERROR_MSG_BUFFER_SIZE: usize = 4096;//one kernel page?!
-                                                 //
-use std::fmt;
-use std::cell::BorrowMutError;
-use std::time::Duration;
+    //const CUSTOM_ERROR_MSG_BUFFER_SIZE: usize = 6;
+    pub const CUSTOM_ERROR_MSG_BUFFER_SIZE: usize = 4096; //one kernel page?!
+                                                          //
+    use std::cell::BorrowMutError;
+    use std::fmt;
+    use std::time::Duration;
 
-#[derive(Debug)]
-pub struct LocationInSource {
-    file: &'static str,
-    line: u32,
-    column: u32,
-}
-
-impl fmt::Display for LocationInSource {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}:{}:{}",
-            file_without_prefix(self.file),
-            self.line, self.column)
+    #[derive(Debug)]
+    pub struct LocationInSource {
+        file: &'static str,
+        line: u32,
+        column: u32,
     }
-}
 
-impl LocationInSource {
-    pub const fn new(file:&'static str, line: u32, column: u32) -> Self {
-        Self {
-            file, line, column
+    impl fmt::Display for LocationInSource {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(
+                f,
+                "{}:{}:{}",
+                file_without_prefix(self.file),
+                self.line,
+                self.column
+            )
         }
     }
 
-}
-
-#[derive(Debug)]
-pub enum MyError {
-    AlreadyBorrowedOrRecursingError {
-        source: BorrowMutError,
-        //where an instance of this error was created, in source code
-        location_of_instantiation: LocationInSource,
-        //custom_message: [u8; CUSTOM_ERROR_MSG_BUFFER_SIZE],
-        custom_message: crate::static_noalloc_msg::NoAllocFixedLenMessageOfPreallocatedSize<CUSTOM_ERROR_MSG_BUFFER_SIZE>,
-        //custom_message: &'static str,
-        //custom_message_len: usize,
-    },
-    TimeoutError {
-        location_of_instantiation: LocationInSource,
-        #[allow(dead_code)]
-        duration: Duration,
-        #[allow(dead_code)]
-        tid: u64,
-        //custom_message: [u8; CUSTOM_ERROR_MSG_BUFFER_SIZE],
-        //custom_message_len: usize,
-        //custom_message: &'static str,
-        //custom_message: crate::static_noalloc_msg::NoAllocFixedLenMessageOfPreallocatedSize< { crate::my_error_things::CUSTOM_ERROR_MSG_BUFFER_SIZE } >,
-        custom_message: crate::static_noalloc_msg::NoAllocFixedLenMessageOfPreallocatedSize<CUSTOM_ERROR_MSG_BUFFER_SIZE>,
-    },
-}
-
-  // Implement the Error trait
-  impl std::error::Error for MyError {
-      fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-          match self {
-              MyError::AlreadyBorrowedOrRecursingError { source, .. } => Some(source),
-              MyError::TimeoutError{ .. } => None,
-          }
-      }
-  }
-
-fn file_without_prefix(file:&str) -> &str {
-    // Remove the prefix of PROJECT_DIR from the file field
-    match file.strip_prefix(PROJECT_DIR) {
-        Some(suffix) => suffix,
-        None => file,
+    impl LocationInSource {
+        pub const fn new(file: &'static str, line: u32, column: u32) -> Self {
+            Self { file, line, column }
+        }
     }
-}
 
-impl fmt::Display for MyError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            MyError::AlreadyBorrowedOrRecursingError {
-                location_of_instantiation,
-                custom_message,
-                //custom_message_len,
-                source,
-            } => {
-                write!(
+    #[derive(Debug)]
+    pub enum MyError {
+        AlreadyBorrowedOrRecursingError {
+            source: BorrowMutError,
+            //where an instance of this error was created, in source code
+            location_of_instantiation: LocationInSource,
+            //custom_message: [u8; CUSTOM_ERROR_MSG_BUFFER_SIZE],
+            custom_message: crate::static_noalloc_msg::NoAllocFixedLenMessageOfPreallocatedSize<
+                CUSTOM_ERROR_MSG_BUFFER_SIZE,
+            >,
+            //custom_message: &'static str,
+            //custom_message_len: usize,
+        },
+        TimeoutError {
+            location_of_instantiation: LocationInSource,
+            #[allow(dead_code)]
+            duration: Duration,
+            #[allow(dead_code)]
+            tid: u64,
+            //custom_message: [u8; CUSTOM_ERROR_MSG_BUFFER_SIZE],
+            //custom_message_len: usize,
+            //custom_message: &'static str,
+            //custom_message: crate::static_noalloc_msg::NoAllocFixedLenMessageOfPreallocatedSize< { crate::my_error_things::CUSTOM_ERROR_MSG_BUFFER_SIZE } >,
+            custom_message: crate::static_noalloc_msg::NoAllocFixedLenMessageOfPreallocatedSize<
+                CUSTOM_ERROR_MSG_BUFFER_SIZE,
+            >,
+        },
+    }
+
+    // Implement the Error trait
+    impl std::error::Error for MyError {
+        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+            match self {
+                MyError::AlreadyBorrowedOrRecursingError { source, .. } => Some(source),
+                MyError::TimeoutError { .. } => None,
+            }
+        }
+    }
+
+    fn file_without_prefix(file: &str) -> &str {
+        // Remove the prefix of PROJECT_DIR from the file field
+        match file.strip_prefix(PROJECT_DIR) {
+            Some(suffix) => suffix,
+            None => file,
+        }
+    }
+
+    impl fmt::Display for MyError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                MyError::AlreadyBorrowedOrRecursingError {
+                    location_of_instantiation,
+                    custom_message,
+                    //custom_message_len,
+                    source,
+                } => {
+                    write!(
                     f,
                     "MyError::AlreadyBorrowedOrRecursingError at location: '{}', custom msg:'{}', generic msg: Already borrowed or recursing error, source error: '{}'",
                     //std::any::type_name::<MyError>(),//::<Self::AlreadyBorrowedOrRecursingError>(),//self,
@@ -100,25 +102,25 @@ impl fmt::Display for MyError {
                     custom_message,
                     source
                 )
-            }
-            MyError::TimeoutError {
-                location_of_instantiation,
-                custom_message,
-                //custom_message_len,
-                ..
-            } => {
-                write!(f, "MyError::TimeoutError at location '{}', custom msg: '{}', generic msg: Timeout after {:?} while trying to find a free slot for thread {}.",
+                }
+                MyError::TimeoutError {
+                    location_of_instantiation,
+                    custom_message,
+                    //custom_message_len,
+                    ..
+                } => {
+                    write!(f, "MyError::TimeoutError at location '{}', custom msg: '{}', generic msg: Timeout after {:?} while trying to find a free slot for thread {}.",
                     //String::from_utf8_lossy(&custom_message[..*custom_message_len]),
                     location_of_instantiation,
                     custom_message,
                     Duration::new(0, 0), 0)
+                }
             }
         }
     }
-}
 
-#[macro_export]
-macro_rules! my_error {
+    #[macro_export]
+    macro_rules! my_error {
     ($variant:path, $message:expr, $($field:ident : $value:expr),* $(,)?) => {{
         let location_of_instantiation=$crate::my_error_things::LocationInSource::new(
             file!(),
@@ -133,70 +135,72 @@ macro_rules! my_error {
         }
     }};
 }
-
-
 } //mod
   //
-//#[deny(unused_must_use)] //no effect
+  //#[deny(unused_must_use)] //no effect
 mod static_noalloc_msg {
 
-#[derive(Debug)]
-pub struct NoAllocFixedLenMessageOfPreallocatedSize<const SIZE: usize> {
-    msg: [u8; SIZE],
-    len: usize,
-    //msg_slice:&'static str, //points into the 'msg' buffer - can't be done this way apparently, XXX: rust?!
-}
-impl<const SIZE: usize> std::fmt::Display for NoAllocFixedLenMessageOfPreallocatedSize<SIZE> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        //let slice=std::str::from_utf8(&self.msg[..self.len]).unwrap_or(concat!("<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<",stringify!(SIZE),">>"));
-        let slice=self.get_msg();
-        write!(f,"{}",slice)
+    #[derive(Debug)]
+    pub struct NoAllocFixedLenMessageOfPreallocatedSize<const SIZE: usize> {
+        msg: [u8; SIZE],
+        len: usize,
+        //msg_slice:&'static str, //points into the 'msg' buffer - can't be done this way apparently, XXX: rust?!
     }
-}
-
-impl<const SIZE: usize> NoAllocFixedLenMessageOfPreallocatedSize<SIZE> {
-    pub fn get_msg(&self) -> &str {
-        let slice=std::str::from_utf8(&self.msg[..self.len]).unwrap_or(concat!("<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<",stringify!(SIZE),">>"));
-        slice
-    }
-    pub const fn new(msg: [u8; SIZE], len: usize) -> Self {
-        //let mut inst=NoAllocFixedLenMessageOfPreallocatedSize {
-        NoAllocFixedLenMessageOfPreallocatedSize {
-            msg,
-            len,
-            //msg_slice: "",
+    impl<const SIZE: usize> std::fmt::Display for NoAllocFixedLenMessageOfPreallocatedSize<SIZE> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            //let slice=std::str::from_utf8(&self.msg[..self.len]).unwrap_or(concat!("<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<",stringify!(SIZE),">>"));
+            let slice = self.get_msg();
+            write!(f, "{}", slice)
         }
-//        //let msg_slice=std::str::from_utf8(&inst.msg[..inst.len]).unwrap_or(concat!("<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<",stringify!(SIZE),">>"));
-//        //inst.msg_slice=msg_slice;
-//        //inst
-//        assert!(len <= SIZE);
-//        let mut ret=NoAllocFixedLenMessageOfPreallocatedSize {
-//            msg,
-//            len,
-//            msg_slice: "",
-//        };
-//        ret.update_slice()
-//    }
-//
-//    fn update_slice(mut self) -> Self {
-//        let msg_slice=std::str::from_utf8(&self.msg[..self.len])
-//            .unwrap_or(
-//                concat!("<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<",stringify!(SIZE),">>"));
-//        //self.msg_slice=msg_slice;
-//        Self {
-//            msg_slice,
-//            ..self
-//        }
-//    }
-//    fn get_msg_as_slice(&self) -> &'static str {
-//        &self.msg_slice
     }
-} // impl
 
-/// format the args (like println!()) into the returned pre-allocated buffer
-//#[deny(unused_must_use)] // no effect!
-#[macro_export]
-macro_rules! format_into_buffer {
+    impl<const SIZE: usize> NoAllocFixedLenMessageOfPreallocatedSize<SIZE> {
+        pub fn get_msg(&self) -> &str {
+            let slice = std::str::from_utf8(&self.msg[..self.len]).unwrap_or(concat!(
+                "<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<",
+                stringify!(SIZE),
+                ">>"
+            ));
+            slice
+        }
+        pub const fn new(msg: [u8; SIZE], len: usize) -> Self {
+            //let mut inst=NoAllocFixedLenMessageOfPreallocatedSize {
+            NoAllocFixedLenMessageOfPreallocatedSize {
+                msg,
+                len,
+                //msg_slice: "",
+            }
+            //        //let msg_slice=std::str::from_utf8(&inst.msg[..inst.len]).unwrap_or(concat!("<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<",stringify!(SIZE),">>"));
+            //        //inst.msg_slice=msg_slice;
+            //        //inst
+            //        assert!(len <= SIZE);
+            //        let mut ret=NoAllocFixedLenMessageOfPreallocatedSize {
+            //            msg,
+            //            len,
+            //            msg_slice: "",
+            //        };
+            //        ret.update_slice()
+            //    }
+            //
+            //    fn update_slice(mut self) -> Self {
+            //        let msg_slice=std::str::from_utf8(&self.msg[..self.len])
+            //            .unwrap_or(
+            //                concat!("<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<",stringify!(SIZE),">>"));
+            //        //self.msg_slice=msg_slice;
+            //        Self {
+            //            msg_slice,
+            //            ..self
+            //        }
+            //    }
+            //    fn get_msg_as_slice(&self) -> &'static str {
+            //        &self.msg_slice
+        }
+    } // impl
+
+    /// format the args (like println!()) into the returned pre-allocated buffer
+    //#[deny(unused_must_use)] // no effect!
+    #[macro_export]
+    macro_rules! format_into_buffer {
     ($fmt:expr, $($arg:tt)*) => {
         //#[must_use] // unused_attributes: `#[must_use]` has no effect when applied to an expression `#[warn(unused_attributes)]` on by default
         //#[deny(unused_must_use)] //no effect here
@@ -235,9 +239,7 @@ macro_rules! format_into_buffer {
         //ret_type
     }};
 } //macro
-
 } //mod
-
 
 use std::cell::RefCell;
 use std::time::Duration;
@@ -245,9 +247,9 @@ use std::time::Duration;
 //#[deny(unused_must_use)] // works ofc, because it applies to call sites!
 fn main() {
     let r = RefCell::new(0);
-    //let inst = r.borrow_mut();//this won't drop the borrow, even tho it's gonna be shadowed.
-    let _inst = r.borrow_mut();//this won't drop the borrow
-    //let _ = r.borrow_mut();//this immediately drops the borrow, so a next borrow mut won't err!
+    //let inst = r.borrow_mut();/*this won't drop the borrow, even tho it's gonna be shadowed.*/
+    let _inst = r.borrow_mut(); /*this won't drop the borrow*/
+    //let _ = r.borrow_mut();/*this immediately drops the borrow, so a next borrow mut won't err!*/
     let inst = r.try_borrow_mut();
 
     //format_into_buffer!("Custom borrow error message with error code {}", 404); //XXX: this gets an unused warning!
@@ -262,8 +264,8 @@ fn main() {
     //    source: inst.err().unwrap()
     //);
 
-    let dur=Duration::new(5, 0);
-    let tid=6;
+    let dur = Duration::new(5, 0);
+    let tid = 6;
     let timeout_error = my_error!(
         my_error_things::MyError::TimeoutError,
         format_into_buffer!("Timeout occurred for thread {} after {:?}", tid, dur),
@@ -274,4 +276,3 @@ fn main() {
     println!("{}", borrow_error);
     println!("{}", timeout_error);
 }
-
