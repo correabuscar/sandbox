6de4626ba6a85b7afcf065c445a7bfb10a3bcbcd
correabuscar <correabuscar+githubcommits@gmail.com>
2024-05-23 23:59:51 +0200

    now they work

--- rust/05_sandbox/enum/enum_variant_name_via_macro_fn/src/main.rs
+++ rust/05_sandbox/enum/enum_variant_name_via_macro_fn/src/main.rs
@@ -5,18 +5,38 @@
 //trait VariantNameAsStr {
 //    fn variant_name_as_str(&self) -> &str;
 //}
+
+#[macro_export]
+macro_rules! replace_with_2_dots {
+    ($($input:tt)*) => {
+        ..
+    };
+}
+
 // https://users.rust-lang.org/t/enum-variant-name-as-str-without-debug-display-proc-macro-or-heap-allocation/111876/
-/// you can only have enums with variants that are either all tuple variants, OR unit and struct variants,
-/// so tuple OR unit+struct
-/// but you can't have all 3 tuple+unit+struct, or tuple+unit
+//
 #[macro_export]
 macro_rules! enum_str {
-    //XXX: arm matches unit variants like Red and struct variants like Red { field1: i32, field2: i64, }, and a mixture of both is supported!
-    ($(#[$attr:meta])* $vis:vis enum $name:ident $(<$($gen:ident),*>)?, $($variant:ident $({ $($field:ident: $ftype:ty),* $(,)? })?),* $(,)?) => {
+    ($(#[$attr:meta])*
+     $vis:vis enum $name:ident $(<$($gen:ident),*>)?,
+     $(
+         $variant:ident
+             $( (
+                 $($tfield:ty),*
+                 $(,)?
+             ) )?
+             $( {
+                 $($sfield:ident: $stype:ty),*
+                 $(,)?
+             } )?
+     ),*
+     $(,)?
+     ) => {
         $(#[$attr])*
         $vis enum $name $(<$($gen),*>)? {
             $(
-                $variant $({ $($field: $ftype),* })?
+                $variant $( ( $($tfield),* ) )?
+                         $( { $($sfield: $stype),* })?
             ),*
         }//enum
 
@@ -26,32 +46,12 @@ macro_rules! enum_str {
                 match self {
                     $(
                         // Handle variants with fields
-                        Self::$variant $({ $($field: _),* })? => stringify!($variant),
-                        //Self::$variant $({..})? => stringify!($variant),
+                        Self::$variant $( ( $crate::replace_with_2_dots!( $($tfield),* ) ) )?
+                                       $( { $($sfield: _),* } )?
+                        => stringify!($variant),
                     )*
-                }
+                }//match
             }//fn
-        }//impl
-    };
-
-    //XXX: arm matches only tuple variants eg. Red(i32,i64,i128) but not Red, nor Red { field:i32 }, so you can't mix them!
-    ($(#[$attr:meta])* $vis:vis enum $name:ident $(<$($gen:ident),*>)?, $($variant:ident $(($($ftype:ty),* $(,)? ))?),* $(,)?) => {
-        $(#[$attr])*
-            $vis enum $name $(<$($gen),*>)? {
-                $(
-                    $variant $(($($ftype),*))?,
-                )*
-            }//enum
-
-        //impl $(<$($gen),*>)? VariantNameAsStr for $name $(<$($gen),*>)? {
-        impl $(<$($gen),*>)? $name $(<$($gen),*>)? {
-            pub const fn variant_name_as_str(&self) -> &str {
-                match self {
-                    $(
-                        Self::$variant(..) => stringify!($variant),
-                    )*
-                }
-            }
         }//impl
     };//arm
 } //macro
@@ -87,7 +87,9 @@ enum_str! {
         custom_message: NoAllocFixedLenMessageOfPreallocatedSize<CUSTOM_ERROR_MSG_BUFFER_SIZE>
     },
     Shie,
-    //NoShie(i32),//XXX: this isn't accepted here!
+    NoShie(i32),
+    RustAnalyzerHi(i32,i8),
+    RustAnalyzerHi2(),
 }
 
 enum_str! {
@@ -121,15 +123,15 @@ enum_str! {
     StructVariant1 {
         field1: i32,
     },
-    //TupleVariant(i32),//XXX: can't match this here!
+    TupleVariant(i32),
 }
 
 enum_str! {
     pub enum Color2<T,G>,
-    //Tee { f: i32 }, // if u use this, then the tuple variant below isn't accepted!
+    Tee { f: i32 },
     Red(T,G), Green(G,i32), Blue(i64,i128,),
-    //Magenta,//XXX: this isn't accepted here!
-    //Foo { field1: i32 }, //XXX: this isn't accepted here!
+    Magenta,
+    Foo { field1: i32 },
 }
 
 fn main() {
