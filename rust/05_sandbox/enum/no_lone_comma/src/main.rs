020fbf6f8162d2c5b099ebcc413483cf0de21b14
correabuscar <correabuscar+githubcommits@gmail.com>
2024-05-28 22:39:40 +0200

    better no lone comma

--- rust/05_sandbox/enum/no_lone_comma/src/main.rs
+++ rust/05_sandbox/enum/no_lone_comma/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused_macros)]
 //the bad way:
 macro_rules! match_generic_params0 {
     // Match (GenericParam `,`)* GenericParam `,`?
@@ -10,7 +11,8 @@ macro_rules! match_generic_params0 {
         }
     };
 }
-macro_rules! match_generic_params {
+// better but still no good to have 2 args:
+macro_rules! match_generic_params1 {
     // Match (GenericParam `,`)* GenericParam `,`?
     (
         $(
@@ -32,6 +34,72 @@ macro_rules! match_generic_params {
             )?
             println!("end");
         }
+    };
+}
+//ok this is good now with 'internal rules' aka https://veykril.github.io/tlborm/decl-macros/patterns/internal-rules.html
+macro_rules! match_generic_params2 {
+    // Match (GenericParam `,`)* GenericParam `,`?
+    (
+        $(
+            $param:ident
+            $(
+              ,  $param2:ident
+            )*
+            $(,)?
+        )?
+    ) => {
+        match_generic_params!(@transcribe $($param $(, $param2)*)*);
+    };
+    (@transcribe $($params:ident),*) => {
+        // This branch will match the input if it conforms to the EBNF
+        {
+            println!("Start");
+            $(
+                println!("Matched param: {}", stringify!($params));
+            )*
+            println!("end");
+        }
+    };
+}
+//better to print them in one line:
+macro_rules! match_generic_params {
+    // Match (GenericParam `,`)* GenericParam `,`?
+    (
+        $(
+            $param:ident
+            $(
+              ,  $param2:ident
+            )*
+            //optionally can end with one comma but only if there was an ident already!
+            $(,)?
+        )?
+    ) => {
+        match_generic_params!(@transcribe $($param $(, $param2)*)*);
+    };
+    (@transcribe ) => {
+        println!("no args");
+    };
+    (@transcribe $($param:ident),*) => {
+        // This branch will match the input if it conforms to the EBNF
+        {
+            println!("Start");
+            //src: DanielKeep https://users.rust-lang.org/t/help-about-writing-macros/111830/2?u=correabuscar
+            println!(
+                concat!("Matched param: ",
+                    $(
+                        match_generic_params!(@replace $param => " {}")
+                    ),*
+                ),
+                $(
+                    stringify!($param),
+                )*
+            );
+            println!("end");
+        }
+    };
+    //src: DanielKeep https://users.rust-lang.org/t/help-about-writing-macros/111830/2?u=correabuscar
+    (@replace $_discard:tt => $($tts:tt)*) => {
+        $($tts)*
     };
 }
 
