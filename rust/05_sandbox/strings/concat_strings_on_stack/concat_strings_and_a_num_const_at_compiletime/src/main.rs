fe12bcbf85cf27da9f883d1c7b23d65abbb2b4dc
correabuscar <correabuscar+githubcommits@gmail.com>
2024-06-09 14:01:48 +0200

    better name, add link to updates

--- rust/05_sandbox/strings/concat_strings_on_stack/concat_strings_and_a_num_const_at_compiletime/src/main.rs
+++ rust/05_sandbox/strings/concat_strings_on_stack/concat_strings_and_a_num_const_at_compiletime/src/main.rs
@@ -1,5 +1,6 @@
 #![feature(const_mut_refs)] // Enable mutable references in const functions
-                            //#![feature(generic_const_exprs)] // warning: the feature `generic_const_exprs` is incomplete and may not be safe to use and/or cause compiler crashes (on rust 1.76.0 gentoo)
+
+//#![feature(generic_const_exprs)] // warning: the feature `generic_const_exprs` is incomplete and may not be safe to use and/or cause compiler crashes (on rust 1.76.0 gentoo)
 
 // so this works at compile time and returns a buffer+len but can't return an &str because it's a reference to something that we've just created locally, not on heap, which even if both it and a ref to it would be returned, the owned would be moved and the ref would thus be stale - compile errors obviously!
 
@@ -22,13 +23,13 @@ pub mod some_mod {
         }
     }
 
-    pub const fn err_msg_buffer_size() -> usize {
+    pub const fn err_msg_max_buffer_size() -> usize {
         80 + 4 // Arbitrary size, should be enough for the err_message, if too low it fails compile time, so just increase it then, but 80+length of SIZE as &str is enough
     }
 
     impl<const SIZE: usize> NoAllocFixedLenMessageOfPreallocatedSize<SIZE> {
-        pub const fn get_err_message() -> ErrMessage<{ err_msg_buffer_size() }> {
-            let mut buffer = [0u8; err_msg_buffer_size()];
+        pub const fn get_err_message() -> ErrMessage<{ err_msg_max_buffer_size() }> {
+            let mut buffer = [0u8; err_msg_max_buffer_size()];
             let mut len = 0;
 
             const PART1: &[u8] = b"<invalid UTF-8 in this instance of ";
@@ -45,7 +46,7 @@ pub mod some_mod {
             ErrMessage { buffer, len }
         }
 
-        //    pub const fn err_msg_buffer_size() -> usize {
+        //    pub const fn err_msg_max_buffer_size() -> usize {
         //        80+4 // Arbitrary size, should be enough for the err_message, if too low it fails compile time, so just increase it then, but 80+length of SIZE as &str is enough
         //    }
     }
@@ -86,7 +87,7 @@ pub mod some_mod {
         let mut j = 0;
 
         while j < bytes_len {
-            buf[i + j] = bytes[j]; // if out of bounds it means err_msg_buffer_size() is too low, like if u're using too big of a SIZE
+            buf[i + j] = bytes[j]; // if out of bounds it means err_msg_max_buffer_size() is too low, like if u're using too big of a SIZE
             j += 1;
         }
 
@@ -94,7 +95,7 @@ pub mod some_mod {
     }
 } //mod
 
-use some_mod::err_msg_buffer_size;
+use some_mod::err_msg_max_buffer_size;
 use some_mod::ErrMessage;
 use some_mod::NoAllocFixedLenMessageOfPreallocatedSize;
 
@@ -108,8 +109,8 @@ fn test_err_msg() {
 }
 
 fn foo() {
-    //const MSG_44: ErrMessage<{ NoAllocFixedLenMessageOfPreallocatedSize::<0>::err_msg_buffer_size()}> = NoAllocFixedLenMessageOfPreallocatedSize::<44>::get_err_message();
-    const MSG_44: ErrMessage<{ err_msg_buffer_size() }> =
+    //const MSG_44: ErrMessage<{ NoAllocFixedLenMessageOfPreallocatedSize::<0>::err_msg_max_buffer_size()}> = NoAllocFixedLenMessageOfPreallocatedSize::<44>::get_err_message();
+    const MSG_44: ErrMessage<{ err_msg_max_buffer_size() }> =
         NoAllocFixedLenMessageOfPreallocatedSize::<44>::get_err_message();
     //let err_message_44:&str = std::str::from_utf8(&MSG_44.buffer[..MSG_44.len]).unwrap();
     let err_message_44: &str = &MSG_44;
@@ -119,8 +120,8 @@ fn foo() {
         "<invalid UTF-8 in this instance of NoAllocFixedLenMessageOfPreallocatedSize::<44>>"
     );
 
-    //const MSG_4096: ErrMessage<{ NoAllocFixedLenMessageOfPreallocatedSize::<0>::err_msg_buffer_size()}> = NoAllocFixedLenMessageOfPreallocatedSize::<4096>::get_err_message();
-    const MSG_4096: ErrMessage<{ err_msg_buffer_size() }> =
+    //const MSG_4096: ErrMessage<{ NoAllocFixedLenMessageOfPreallocatedSize::<0>::err_msg_max_buffer_size()}> = NoAllocFixedLenMessageOfPreallocatedSize::<4096>::get_err_message();
+    const MSG_4096: ErrMessage<{ err_msg_max_buffer_size() }> =
         NoAllocFixedLenMessageOfPreallocatedSize::<4096>::get_err_message();
     //let err_message_4096: &str = std::str::from_utf8(&MSG_4096.buffer[..MSG_4096.len]).unwrap();
     let err_message_4096: &str = &MSG_4096;
