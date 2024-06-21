3a197dc1500628e72562bf3912b5c3f6057a433b
correabuscar <correabuscar+githubcommits@gmail.com>
2024-06-21 09:33:47 +0200

    move the 'sup' to its proper non-testing place

--- rust/05_sandbox/error/error_propagation_with_own_msg_and_location2/src/main.rs
+++ rust/05_sandbox/error/error_propagation_with_own_msg_and_location2/src/main.rs
@@ -1253,8 +1253,8 @@ static PANIC_ON_ALLOC:AtomicBool=AtomicBool::new(false);
 //#[deny(unused_must_use)] // works ofc, because it applies to call sites!
 //fn main() {
 fn main() -> Result<(), my_error_things::MyError> {
-    PANIC_ON_ALLOC.store(true, Ordering::Relaxed);//from now on, panic on any memory allocations!
     println!("sup");//this allocates on first use a buffer(of 1k) for stdout.
+    PANIC_ON_ALLOC.store(true, Ordering::Relaxed);//from now on, panic on any memory allocations!
 
     let res = some_fn();
     let err = res.err().unwrap();
