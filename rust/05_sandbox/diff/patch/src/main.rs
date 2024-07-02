7244ba52b0e2617a086843f5ed5b9114a7c954c5
correabuscar <correabuscar+githubcommits@gmail.com>
2024-07-02 05:21:22 +0200

    improvements to diff/patch projects
    and simplified poc patch

--- rust/05_sandbox/diff/patch/src/main.rs
+++ rust/05_sandbox/diff/patch/src/main.rs
@@ -5,9 +5,18 @@ use std::str;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
-        eprintln!("Usage: patch <original_file> <patch_file> <output_file>");
+    if args.len() > 5 || args.len() < 4 {
+        eprintln!("Usage: patch <original_file> <patch_file> <output_file> true/false");
+        eprintln!("true/false is for unambiguous");
         std::process::exit(1);
+    }
+    #[allow(unused_variables)]
+    let unambiguous:bool;
+    #[allow(unused_assignments)]
+    if args.len()==5 {
+        unambiguous=args[4].parse().expect("Failed to parse arg number 5 into bool: true/false");
+    } else {
+        unambiguous=false;
     }
 
     let original_file = fs::read(&args[1]).expect("Failed to read original file");
@@ -17,7 +26,9 @@ fn main() {
     let patch_str = str::from_utf8(&patch_file).expect("Failed to convert patch file to string");
 
     let patch = Patch::from_str(patch_str).expect("Failed to parse patch file");
-    let patched_str = apply(original_str, &patch).expect("Failed to apply patch");
+    let patched_str = apply(original_str, &patch
+        ,unambiguous
+        ).expect("Failed to apply patch");
 
     fs::write(&args[3], patched_str.as_bytes()).expect("Failed to write output file");
 }
