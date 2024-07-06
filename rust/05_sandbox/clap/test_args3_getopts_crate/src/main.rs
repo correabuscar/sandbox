e6532294884d79e77b925db13f883444108e1510
correabuscar <correabuscar+githubcommits@gmail.com>
2024-07-06 14:41:30 +0200

    imp. example and use local git version of getopts

--- rust/05_sandbox/clap/test_args3_getopts_crate/src/main.rs
+++ rust/05_sandbox/clap/test_args3_getopts_crate/src/main.rs
@@ -1,15 +1,15 @@
 //initial example from: https://docs.rs/getopts/0.2.21/getopts/index.html#example
-extern crate getopts;
-use getopts::Options;
+//extern crate getopts;
+use getopts::{Options, Occur, HasArg};
 use std::env;
 
-fn do_work(inp: &str, out: Option<String>) {
-    println!("{}", inp);
-    match out {
-        Some(x) => println!("{}", x),
-        None => println!("No Output"),
-    }
-}
+//fn do_work(inp: &str, out: Option<String>) {
+//    println!("{}", inp);
+//    match out {
+//        Some(x) => println!("{}", x),
+//        None => println!("No Output"),
+//    }
+//}
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} FILE [options]", program);
@@ -21,7 +21,10 @@ fn main() {
     let program = args[0].clone();
 
     let mut opts = Options::new();
-    opts.optopt("o", "", "set output file name", "NAME");
+    /* Single-character options are expected to appear on the command line with a single preceding dash; multiple-character options are expected to be proceeded by two dashes. Options that expect an argument accept their argument following either a space or an equals sign. Single-character options don't require the space. */
+    //FIXME: well that's not true because --unified 34 is taken as --unified and the free arg 34, and not like --unified=34 !
+    opts.opt("u", "unified", "output NUM (default 3) lines of unified contex", "NUM", HasArg::Maybe, Occur::Multi);
+    opts.opt("U", "unified", "output NUM (default 3) lines of unified contex", "NUM", HasArg::Maybe, Occur::Multi);
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -31,12 +34,20 @@ fn main() {
         print_usage(&program, opts);
         return;
     }
-    let output = matches.opt_str("o");
-    let input = if !matches.free.is_empty() {
-        matches.free[0].clone()
-    } else {
-        print_usage(&program, opts);
-        return;
+    let context_length = match matches.opt_str("u") { //this catches the uppercase -U too! unclear why, maybe due to --unified being same?
+        Some(cl) => cl.parse::<i32>().expect(&format!("Context length '{}' isn't an i32 number.", cl)),
+        None => 3,
     };
-    do_work(&input, output);
+    println!("Context length: {}", context_length);
+    if context_length < 0 {
+        panic!("negative context length given");
+    }
+    println!("Free: {} {:?}",matches.free.len(), matches.free);
+//    let input = if !matches.free.is_empty() {
+//        matches.free[0].clone()
+//    } else {
+//        print_usage(&program, opts);
+//        return;
+//    };
+    //do_work(&input, output);
 }
