310ed876b7a53ddc912b50107cbe13859dda42e3
correabuscar <correabuscar+githubcommits@gmail.com>
2024-07-11 23:39:14 +0200

    into functions

--- bash/ps/our_caller_tree/show_our_caller_tree.bash
+++ bash/ps/our_caller_tree/show_our_caller_tree.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Array to store process information
-declare -a process_tree
+function get_callers_pstree() {
+  # Array to store process information
+  local -a process_tree=()
 
-# Set to store processed PIDs
-declare -A processed_pids
+  # Set to store processed PIDs
+  local -A processed_pids
 
-# Function to retrieve process information recursively
-build_process_tree() {
-  #shopt -s extglob  # Enable extended globbing
+  # Function to retrieve process information recursively
+  function build_process_tree() {
+    #shopt -s extglob  # Enable extended globbing
     local pid=$1
     local indent=$2
     #echo "New: $# $*"
@@ -22,31 +23,31 @@ build_process_tree() {
 
 
 
-    # Check if we've already processed this PID to avoid infinite loops
-    if [[ -n "${processed_pids[$trimmed_pid]}" ]]; then
-      echo "!! Avoided infinite loop for '$pid' due to bad coding!" >&2
-      return 2
-    fi
+      # Check if we've already processed this PID to avoid infinite loops
+      if [[ -n "${processed_pids[$trimmed_pid]}" ]]; then
+        echo "!! Avoided infinite loop for '$pid' due to bad coding!" >&2
+        return 2
+      fi
 
-    # Mark this PID as processed
-    processed_pids[$trimmed_pid]=1
+      # Mark this PID as processed
+      processed_pids[$trimmed_pid]=1
 
-    # Read command line arguments from /proc/PID/cmdline into an array
-    local cmdline_args=()
-    while IFS= read -r -d $'\0' arg; do
+      # Read command line arguments from /proc/PID/cmdline into an array
+      local cmdline_args=()
+      while IFS= read -r -d $'\0' arg; do
         cmdline_args+=("$arg")
-    done < "/proc/$trimmed_pid/cmdline"
+      done < "/proc/$trimmed_pid/cmdline"
 
-    # Determine the command (first argument) and its arguments count
-    #local cmd="${cmdline_args[0]}"
-    local args_count="${#cmdline_args[@]}"
+      # Determine the command (first argument) and its arguments count
+      #local cmd="${cmdline_args[0]}"
+      local args_count="${#cmdline_args[@]}"
 
-    ## Get process command line
-    #local cmd
-    ##cmd="$(ps -o cmd= -p "$pid" 2>/dev/null)"
-    #cmd="$(ps -o cmd= -p "$trimmed_pid")" # 2>/dev/null)"
+      ## Get process command line
+      #local cmd
+      ##cmd="$(ps -o cmd= -p "$pid" 2>/dev/null)"
+      #cmd="$(ps -o cmd= -p "$trimmed_pid")" # 2>/dev/null)"
 
-    if test "$args_count" -gt "0"; then
+      if test "$args_count" -gt "0"; then
         # Store process information in the array
         #process_tree+=("${indent}PID: '$pid' - Command: '$cmd'" )
         #process_tree+=("${indent}'$cmd'" )
@@ -60,32 +61,36 @@ build_process_tree() {
         #used $pid with spaces on purpose - doesn't apply anymore, $pid is trimmed too.
         process_tree+=("${indent}'$pid'-'$args_count'-${all_args}")
 
-        # Find parent PID
-        local ppid
-        #ppid="$(ps -o ppid= -p "$pid" 2>/dev/null)"
-        #ppid="$(ps -o ppid= -p "$trimmed_pid" 2>/dev/null)"
-        ppid="$(awk '{print $4}' "/proc/$pid/stat")"
-        if test "$ppid" != "0"; then
-          local trimmed_Ppid
-          trimmed_Ppid="${ppid#"${ppid%%[![:space:]]*}"}"   # Remove leading whitespace
-          trimmed_Ppid="${trimmed_Ppid%"${trimmed_Ppid##*[![:space:]]}"}"   # Remove trailing whitespace
-          #echo "trim2='$trimmed_Ppid'"
+          # Find parent PID
+          local ppid
+          #ppid="$(ps -o ppid= -p "$pid" 2>/dev/null)"
+          #ppid="$(ps -o ppid= -p "$trimmed_pid" 2>/dev/null)"
+          ppid="$(awk '{print $4}' "/proc/$pid/stat")"
+          if test "$ppid" != "0"; then
+            local trimmed_Ppid
+            trimmed_Ppid="${ppid#"${ppid%%[![:space:]]*}"}"   # Remove leading whitespace
+            trimmed_Ppid="${trimmed_Ppid%"${trimmed_Ppid##*[![:space:]]}"}"   # Remove trailing whitespace
+            #echo "trim2='$trimmed_Ppid'"
 
 
-        # Recursively call function for parent process
-        #if [ "$trimmed_Ppid" != "1" ] && [ "$trimmed_Ppid" != "$trimmed_pid" ]; then
-            build_process_tree "$ppid" "$indent  "
-        fi
-    fi
-}
+          # Recursively call function for parent process
+          #if [ "$trimmed_Ppid" != "1" ] && [ "$trimmed_Ppid" != "$trimmed_pid" ]; then
+          build_process_tree "$ppid" "$indent  "
+          fi
+      fi
+    }
 
-# Start with our own PID
-build_process_tree "$$" ""
+  # Start with our own PID
+  build_process_tree "$$" ""
 
-# Print the process tree
-str="Our callers tree:"$'\n'
-for line in "${process_tree[@]}"; do
+  # Print the process tree
+  str="Our callers tree:"$'\n'
+  for line in "${process_tree[@]}"; do
     #echo "$line"
     str+="${line}"$'\n'
-done
-echo -n "$str"
+  done
+  echo -n "$str"
+}
+
+get_callers_pstree "$@"
+
